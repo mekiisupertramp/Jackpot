069169e479c651485e7bf2802f47b55225430abb
pierre.buffo <pierre.buffo@etu.hesge.ch>
2017-01-29 11:57:45 +0100

    encore du débug

--- libs/signals.c
+++ libs/signals.c
@@ -33,32 +33,32 @@ void *signalReceiver(void *threadData) {
             case SIGINT:  // stop the current wheel
                 if (tdata->gameState == ROLLING) {
                     tdata->wheels[0].condMutex->var++;
-                    if(pthread_create(&tTimer,NULL,timer,tdata) != 0){
-                      fprintf(stderr, "sign pthread_create failed !\n");
-                      return NULL;
+                    if (pthread_create(&tTimer, NULL, timer, tdata) != 0) {
+                        fprintf(stderr, "sign pthread_create failed !\n");
+                        return NULL;
                     }
                     pthread_cond_broadcast(&tdata->wheels[0].condMutex->cond);
                 }
-            break;
+                break;
             case SIGTSTP: // insert coin
                 tdata->gameState = ROLLING;
                 pthread_mutex_lock(&(tdata->wheels[0].condMutex->m));
-                tdata->wheels[0].condMutex->var=0;
-                if(pthread_create(&tTimer,NULL,timer,tdata) != 0){
-                  fprintf(stderr, "sign pthread_create failed !\n");
-                  return NULL;
+                tdata->wheels[0].condMutex->var = 0;
+                if (pthread_create(&tTimer, NULL, timer, tdata) != 0) {
+                    fprintf(stderr, "sign pthread_create failed !\n");
+                    return NULL;
                 }
                 pthread_cond_broadcast(&tdata->wheels[0].condMutex->cond);
                 pthread_mutex_unlock(&(tdata->wheels[0].condMutex->m));
                 tdata->coins++;
-            break;
+                break;
             case SIGQUIT: // quit the game
                 quit = 1;
-            break;
+                break;
             case SIGALRM: // waiting 5 seconds at the end
                 sleep(5);
                 tdata->gameState = WAITING;
-            break;
+                break;
         }
     } while (quit == 0);
     pthread_mutex_lock(&(tdata->wheels[0].condMutex->m));
@@ -75,10 +75,11 @@ void *signalReceiver(void *threadData) {
  * @param threadData - struct containing the datas
  * @return NULL
  */
-void *timer(void *threadData){
-  controller_t *tdata = (controller_t *) (threadData);
-  sleep(3);
-  if(tdata->wheels[0].condMutex->var > tdata->wheels[0].id)
-      kill(getpid(),SIGINT);
-  return NULL;
+void *timer(void *threadData) {
+    controller_t *tdata = (controller_t *) (threadData);
+    int tmp = tdata->wheels[0].condMutex->var;
+    sleep(3);
+    if (tdata->wheels[0].condMutex->var == tmp)
+        kill(getpid(), SIGINT);
+    return NULL;
 }
